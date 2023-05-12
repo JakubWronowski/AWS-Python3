76f4c6aa373677df2da1dc090e0262eb51b59a48
JakubWronowski <132200637+JakubWronowski@users.noreply.github.com>
2023-05-12 21:58:14 +0200

    Add files via upload

---
+++ PythonCodeToRestartInstanceEC2.py
@@ -0,0 +1,9 @@
+import json
+import boto3
+region = 'eu-west-1'
+instances = ['my-instance']
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+  ec2.start_instances(InstanceIds=instances)
+  print('started your instances: ' + str(instances))
